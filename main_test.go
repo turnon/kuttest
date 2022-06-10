b7833ee6f68a0cc17708b2702fbd801cd4e78d93
ken <block24block@gmail.com>
2022-06-10 13:13:43 +0800

    timing

--- main_test.go
+++ main_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"os"
 	"strconv"
 	"sync"
 	"testing"
+	"time"
 )
 
 func TestRandGenerator(t *testing.T) {
@@ -22,6 +24,8 @@ func TestPostLinks(t *testing.T) {
 	var wg sync.WaitGroup
 	wg.Add(lc)
 
+	start := time.Now()
+
 	for i := 0; i < lc; i++ {
 		pool <- func() {
 			num := strconv.FormatUint(randGen(), 10)
@@ -34,17 +38,26 @@ func TestPostLinks(t *testing.T) {
 	}
 
 	wg.Wait()
+	t.Log(time.Now().Sub(start))
 }
 
 func TestGetRedirect(t *testing.T) {
-	randGen := randGenerator()
 	pool := generatePool()
 	lc := loopCount()
+	num := os.Getenv("SHORT")
+
+	var wg sync.WaitGroup
+	wg.Add(lc)
+
+	start := time.Now()
 
 	for i := 0; i < lc; i++ {
 		pool <- func() {
-			num := strconv.FormatUint(randGen(), 10)
 			getRedirect(num)
+			wg.Done()
 		}
 	}
+
+	wg.Wait()
+	t.Log(time.Now().Sub(start))
 }
